ad2f3845347fccb650d8995220a6aa6a8495c6e2
digetl <dave@digetl.co.uk>
2020-06-27 14:12:05 +0100

    Can add to shop.

--- src/test/java/Shop/ShopTest.java
+++ src/test/java/Shop/ShopTest.java
@@ -1,5 +1,7 @@
 package Shop;
 
+import Items.Instruments.Percussion.Percussion;
+import Items.Instruments.Percussion.PercussionInst;
 import Items.Item;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,15 +14,22 @@ public class ShopTest {
 
     private Shop shop;
     private ArrayList<Item> itemsForSale;
+    private Percussion snaredrum;
 
     @Before
     public void before() {
+        snaredrum = new Percussion("Ludwig big snare", PercussionInst.SNAREDRUM1);
         shop = new Shop("High Notes Music Shop", itemsForSale);
     }
 
     @Test
     public void getShopName() {
         assertEquals("High Notes Music Shop", shop.getShopName());
+    }
+
+    @Test
+    public void canAddItemsToStock() {
+        shop.addItemToStock(snaredrum);
     }
 
 }
