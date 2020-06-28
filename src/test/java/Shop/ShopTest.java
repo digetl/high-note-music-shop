3f4d937e79586ee158638f1797150933de6c08c9
Dave Campbell <dave@digetl.co.uk>
2020-06-28 09:11:11 +0100

    test - calculateMarkupAllStock - failed

--- src/test/java/Shop/ShopTest.java
+++ src/test/java/Shop/ShopTest.java
@@ -20,7 +20,7 @@ public class ShopTest {
     @Before
     public void before() {
         snaredrum = new Percussion("Ludwig big snare", 200.00, 100.00, PercussionInst.SNAREDRUM1);
-        item1 = new Item("Snare1", 50.00,100.00);
+        item1 = new Item("DrumSticks", 10.00,20.00);
         shop = new Shop("High Notes Music Shop", itemsForSale);
     }
 
@@ -45,7 +45,17 @@ public class ShopTest {
 
     @Test
     public void canCalculateMarkup() {
-        assertEquals(50, shop.calculateMarkup(item1), 0.01);
+        assertEquals(10, shop.calculateMarkup(item1), 0.01);
+    }
+
+    @Test
+    public void canCalculateMarkUpOnAllStock__Instruments_Only(){
+
+        shop.addItemToStock(snaredrum);
+        shop.addItemToStock(snaredrum);
+        shop.removeItemFromStock(snaredrum);
+        assertEquals(0, shop.calculateMarkupAllStock());
+
     }
 
 }
